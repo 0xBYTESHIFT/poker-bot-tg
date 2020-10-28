da1599b4f804003c2c1ddb426bc73e36e72d1b26
0xBYTESHIFT <0xBYTESHIFT@gmail.com>
2020-10-28 23:45:05 +0300

    added more docs

--- include/core/nameable.h
+++ include/core/nameable.h
@@ -3,16 +3,30 @@
 #include "core/property.h"
 
 namespace bot{
+
+/**
+ * Class to hold name of derived entities.
+ * */ 
 class nameable{
 public:
-    using name_t = std::string;
+    using name_t = std::string; /**< Name type define */
 protected:
-    nameable(const name_t name = "unnamed")
-        :name(name){}
+    /**
+     * Nameable constructor.
+     * @param name name to save, default = "unnamed"
+     * */
+    nameable(const name_t name = "unnamed");
 public:
-
-    virtual ~nameable(){}
-    property<name_t> name;
+    /**
+     * Virtual destructor
+     * */
+    virtual ~nameable();
+    property<name_t> name; /**< Name of derived class object */ 
 };
+
+nameable::nameable(const name_t name)
+    :name(name)
+{}
+nameable::~nameable(){}
 
 };
--- include/core/user.h
+++ include/core/user.h
@@ -8,18 +8,33 @@ namespace bot{
 
 class room;
 
+/**
+ * User's class to hold their's properties.
+ * */
 class user:public nameable, public identifyable{
 public:
-    using room_ptr = std::shared_ptr<room>;
-    using token_t = std::string;
+    using room_ptr = std::shared_ptr<room>; /**< define for room pointer */
+    using token_t = std::string; /**< define for token's type */
 
-    user(id_t id):identifyable(id){}
+    /**
+     * User's constructor.
+     * @param id id of a user, defined by Telegram
+     * */
+    user(id_t id);
 
-    property<room_ptr> room = nullptr;
-    property<token_t> token = "";
+    property<room_ptr> room = nullptr; /**< Room where user currently is */
+    property<token_t> token = ""; /**< User's token to refer to them in commands */
 
+    /**
+     * User's description, consists of user's name and token.
+     * @returns user's description
+     * */
     virtual std::string desc()const;
 };
+
+user::user(id_t id)
+    :identifyable(id)
+{}
 
 std::string user::desc()const{
     return name()+"["+token()+"]";

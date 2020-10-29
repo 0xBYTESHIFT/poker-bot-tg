74b219857cf3acf64cfe4957311cc44496287df2
0xBYTESHIFT <0xBYTESHIFT@gmail.com>
2020-10-29 23:36:56 +0300

    added server's docs

--- include/core/server.h
+++ include/core/server.h
@@ -12,21 +12,28 @@
 #include "core/utils.h"
 
 namespace bot{
+/**
+ * Utility class to generate random tokens.
+ * */
 class token_generator{
-    static inline std::set<std::string> p_tokens;
-    const static size_t p_token_len = 8; 
-    const static inline std::string p_alphabet =
-        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-    static inline std::random_device rd;
-    static inline std::mt19937 p_gen = std::mt19937(rd());
-    static inline std::uniform_int_distribution<unsigned> dist =
-        std::uniform_int_distribution<unsigned>(0, 51); //p_alphabet.size()-1
+    static inline std::set<std::string> p_tokens; /**< Set to keep already generated tokens */
+    const static size_t p_token_len = 8;  /**< Define for token's length */ 
+    const static inline std::string p_alphabet = /**< Alphabet for generating tokens */
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
 public:
+    /**
+     * Func that generates a random unique token.
+     * @returns a token
+     * */
     static std::string gen();
 };
 
 std::string token_generator::gen(){
+    static std::random_device rd;
+    static std::mt19937 p_gen = std::mt19937(rd());
+    static std::uniform_int_distribution<unsigned> dist =
+        std::uniform_int_distribution<unsigned>(0, p_alphabet.size()-1);
+
     std::string result(p_token_len, ' ');
     bool end = false;
     do{
@@ -40,25 +47,60 @@ std::string token_generator::gen(){
     return result;
 };
 
+/**
+ * Server class to hold users and rooms
+ * */
 class server{
-    static inline id_t p_last_room_id = 0;
+    static inline id_t p_last_room_id = 0; /**< Last room id to keep generated room's unique */
 protected:
-    logger &lgr;
+    logger &lgr; /**< Logger reference */
+    /**
+     * Function to return new room's id
+     * */
     id_t p_get_room_id();
 public:
-    using room_cont = std::vector<room_ptr>;
-    using user_cont = std::map<identifyable::id_t, user_ptr>;
+    using room_cont = std::vector<room_ptr>; /**< Define for rooms container */ 
+    using user_cont = std::map<identifyable::id_t, user_ptr>; /**< Define for users container */
 
-    property<room_cont> rooms = {};
-    property<room_ptr> lobby = nullptr;
-    property<user_cont> users = {};
+    property<room_cont> rooms = {}; /**< Rooms' pointers container */
+    property<room_ptr> lobby = nullptr; /**< Pointer to the lobby room */
+    property<user_cont> users = {}; /**< Users' container */
 
+    /**
+     * Default constructor, initializes the lobby
+     * */
     server();
+    /**
+     * Function to find a user by their's id.
+     * @returns user_ptr if they are found
+     * */
     virtual user_ptr get_user(id_t id)const;
+    /**
+     * Function to find a room by it's token.
+     * @returns room_ptr if it's found
+     * */
     virtual room_ptr get_room(const room::token_t &token)const;
+    /**
+     * Function to create a room when user requested it.
+     * @param user ptr to a user that requester room creation.
+     * @returns pointer to a created room
+     * */
     virtual room_ptr create_room(user_ptr user);
+    /**
+     * Function to be called when user connects to the server.
+     * @param user ptr to a user that connected to the server.
+     * */
     virtual void on_user_connect(user_ptr user);
+    /**
+     * Function to be called when user disconnects to the server.
+     * @param user ptr to a user that diconnected to the server.
+     * */
     virtual void on_user_disconnect(user_ptr user);
+    /**
+     * Function to be called when room became empty.
+     * By default, this function deletes the room.
+     * @param room ptr to a room that became empty
+     * */
     virtual void on_room_empty(room_ptr room);
 };
 

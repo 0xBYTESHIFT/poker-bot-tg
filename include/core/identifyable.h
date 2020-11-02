4418da245dc060273d6db1d522b81dec67248672
0xBYTESHIFT <0xBYTESHIFT@gmail.com>
2020-11-02 16:36:07 +0300

    added id's class docs

--- include/core/identifyable.h
+++ include/core/identifyable.h
@@ -3,15 +3,25 @@
 #include "core/property.h"
 
 namespace bot{
+/**
+ * Identifyable class interface.
+ * */
 class identifyable{
 public:
-    using id_t = std::size_t;
+    using id_t = std::size_t; /**< Id's type define */
 protected:
+    /**
+     * Constructor, takes and stores an id.
+     * @param id id to store.
+     * */
     identifyable(id_t id):id(id){}
 public:
-
+    /**
+     * Virtual destructor.
+     * Needed for polymorphism to work.
+     * */
     virtual ~identifyable(){}
-    const property<id_t> id;
+    const property<id_t> id; /**< Property that stores an id. */
 };
 
 };

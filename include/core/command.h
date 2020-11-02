5d7af2fa6926c65409b21476185df8153d429f72
0xBYTESHIFT <0xBYTESHIFT@gmail.com>
2020-11-02 16:33:06 +0300

    added command's docs

--- include/core/command.h
+++ include/core/command.h
@@ -5,27 +5,61 @@
 
 namespace bot{
 
+/**
+ * Bot's command class.
+ * */
 class command{
 public:
-    using name_t = std::string;
-    using callback_t = std::function<void(mes_ptr)>;
+    using name_t = std::string; /**< Name type define. */
+    using callback_t = std::function<void(mes_ptr)>; /**< Callback type define. */
 
+    /**
+     * Command's constructor.
+     * @param name name of the command.
+     * @param desc description of the command.
+     * @param args vector for command's arguments names.
+     * @param callback callback to be called when command is entered correctly.
+     * */
     command(const name_t &name,
         const std::string &desc,
         const std::vector<std::string> &args,
         const callback_t &callback);
 
+    /**
+     * Returns info about using a command. Example: "/kick [user_token]".
+     * @returns string with info about command's usage.
+     * */
     auto usage()const -> std::string;
+    /**
+     * Returns name of a command. Example: "kick".
+     * @returns string with the name of a command.
+     * */
     auto cmd_word()const -> const std::string&;
+    /**
+     * Returns description of a command. Example: "kicks unwanted player out".
+     * @returns string with the description of a command.
+     * */
     auto desc()const -> const std::string&;
+    /**
+     * Returns vector of command's arguments names. Example: {"user_token",}.
+     * @returns vector of strings, representing command's arguments names.
+     * */
     auto args()const -> const std::vector<std::string>&;
+    /**
+     * Returns callback of a command.
+     * @returns callback of a command.
+     * */
     auto callback()const -> callback_t;
+    /**
+     * Call's command's callback with user's message.
+     * @param mes message from user with command's call.
+     * */
     void invoke(mes_ptr mes);
 protected:
-    const std::string m_desc;
-    const std::string m_cmd_word;
-    const std::vector<std::string> m_args;
-    const callback_t m_callback;
+    const std::string m_desc; /**< Description of a command. */
+    const std::string m_cmd_word; /**< Command's name. */
+    const std::vector<std::string> m_args; /**< Command's args names. */
+    const callback_t m_callback; /**< Command's callback. */
 };
 
 command::command(const name_t &name, const std::string &desc,

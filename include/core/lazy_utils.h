342bedebd2797116c13522d0c853cd6ce1ae1bcd
0xBYTESHIFT <0xBYTESHIFT@gmail.com>
2020-11-11 22:09:36 +0300

    added betting and small improvements

--- include/core/lazy_utils.h
+++ include/core/lazy_utils.h
@@ -11,6 +11,18 @@ using predicate_unary = std::function<bool(const T&)>;
 template<class T>
 using predicate_binary = std::function<bool(const T&, const T&)>;
 
+template<class T, class Arg>
+auto stat_cast(Arg ptr){ return std::static_pointer_cast<T>(ptr); }
+
+template<class T, class Arg>
+auto dyn_cast(Arg ptr){ return std::dynamic_pointer_cast<T>(ptr); }
+
+template<class T, class Arg>
+auto dyn_cast(std::unique_ptr<Arg> &ptr){ return dynamic_cast<T*>(ptr.get()); }
+
+template<class T, class Arg>
+auto dyn_cast(const std::unique_ptr<Arg> &ptr){ return dynamic_cast<T*>(ptr.get()); }
+
 template<class T, class Val = typename T::value_type>
 auto find(const T& cont, const Val& val) {
     auto it = std::find(cont.begin(), cont.end(), val);
@@ -53,6 +65,15 @@ template<class T, class Pred>
 auto contains_if(const T& cont, Pred pred) {
     auto it = find_if(cont, pred);
     return it != cont.end();
+}
+
+template<class Cont, class Val = typename Cont::value_type>
+size_t index(const Cont &cont, const Val &val){
+    auto it = find(cont, val);
+    if(it == cont.end()){
+        return -1;
+    }
+    return std::distance(cont.begin(), it);
 }
 
 template<class T, class Func, class... Args>

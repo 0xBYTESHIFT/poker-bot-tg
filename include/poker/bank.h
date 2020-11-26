3a35ae916fb16dac2a58812907f7f932ea1de3b8
0xBYTESHIFT <0xBYTESHIFT@gmail.com>
2020-11-26 19:45:18 +0300

    added sanitizer and silenced a error

--- include/poker/bank.h
+++ include/poker/bank.h
@@ -47,27 +47,21 @@ bank::bank(const size_t& size) {
 
 auto bank::get_coins(const std::size_t& count) -> bank::coins_t {
     auto& coins = this->coins();
-    auto i      = count;
     coins_t result;
     if(count > coins.size()) {
         auto mes = "Attempt to get " + std::to_string(count) +
                    "coins from a bank with size " + std::to_string(count);
         throw std::runtime_error(mes);
     }
-    do {
-        auto it = coins.begin() + i - count;
-        auto c  = std::move(*it);
-        result.emplace_back(std::move(c));
-        i--;
-    } while(i != 0);
+    result.reserve(count);
+    auto it = coins.begin() + count;
+    std::move(coins.begin(), it, std::back_inserter(result));
     coins.erase(coins.begin(), coins.begin() + count);
     return result;
 }
 void bank::add_coins(bank::coins_t& coins) {
     this->coins().reserve(this->coins().size() + coins.size());
-    for(auto& c: coins) {
-        this->coins().emplace_back(std::move(c));
-    }
+    std::move(coins.begin(), coins.end(), std::back_inserter(this->coins()));
     coins.clear();
     coins.shrink_to_fit();
 }
